0a26c000cf56ccad4ddb5da9369bb10724d229ef
Thibaut Vial <github@thibautvial.com>
2014-03-27 00:04:09 -0700

    First checkin of the project itself as a Pivot style app

---
+++ Article.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiSpeak
+{
+	/// <summary>
+	/// This class contains all the info relative to an article
+	/// </summary>
+	public class Article
+	{
+		/// <summary>
+		/// The term that was used for the search
+		/// </summary>
+		public string SearchTerm;
+
+		/// <summary>
+		/// The title of the article in WikiPedia
+		/// </summary>
+		public string Title;
+
+		/// <summary>
+		/// An excerpt
+		/// </summary>
+		public string Excerpt;
+
+	}
+}
---
+++ ViewModels/ArticleViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace WikiSpeak.ViewModels
+{
+	public class ArticleViewModel : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Uhnderlying article object
+		/// </summary>
+		public Article Article
+		{
+			get
+			{
+				return _article;
+			}
+			set
+			{
+				if (_article != value)
+				{
+					_article = value;
+					NotifyPropertyChanged("Title");
+					NotifyPropertyChanged("Excerpt");
+				}
+			}
+		}
+		private Article _article;
+
+		/// <summary>
+		/// Gets the title of the article
+		/// </summary>
+		public string Title
+		{
+			get
+			{
+				return (_article != null ? _article.Title : string.Empty);
+			}
+		}
+
+		/// <summary>
+		/// Gets the excerpt of the article
+		/// </summary>
+		public string Excerpt
+		{
+			get
+			{
+				return (_article != null ? _article.Excerpt : string.Empty);
+			}
+		}
+
+		/// <summary>
+		/// Event for property changed notification
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Helper method for property change notification
+		/// </summary>
+		/// <param name="propertyName"></param>
+		private void NotifyPropertyChanged(String propertyName)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (null != handler)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
